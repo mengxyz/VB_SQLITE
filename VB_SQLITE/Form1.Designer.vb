a5972c3fd3558faee3751ee3b08c626a41861851
mengxyz <36928202+mengxyz@users.noreply.github.com>
2019-03-26 19:27:14 +0700

    Initial commit

---
+++ VB_SQLITE/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.label6 = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.lbl = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.cmbStage = New System.Windows.Forms.ComboBox()
+        Me.cmbGender = New System.Windows.Forms.ComboBox()
+        Me.cmbStroke = New System.Windows.Forms.ComboBox()
+        Me.cmbDistance = New System.Windows.Forms.ComboBox()
+        Me.cmbSwimmer = New System.Windows.Forms.ComboBox()
+        Me.button5 = New System.Windows.Forms.Button()
+        Me.button4 = New System.Windows.Forms.Button()
+        Me.button3 = New System.Windows.Forms.Button()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.txtPlace = New System.Windows.Forms.TextBox()
+        Me.txtTime = New System.Windows.Forms.TextBox()
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dataGridView1
+        '
+        Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridView1.Location = New System.Drawing.Point(21, 342)
+        Me.dataGridView1.Name = "dataGridView1"
+        Me.dataGridView1.Size = New System.Drawing.Size(669, 256)
+        Me.dataGridView1.TabIndex = 40
+        '
+        'label6
+        '
+        Me.label6.AutoSize = True
+        Me.label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label6.Location = New System.Drawing.Point(142, 255)
+        Me.label6.Name = "label6"
+        Me.label6.Size = New System.Drawing.Size(48, 20)
+        Me.label6.TabIndex = 39
+        Me.label6.Text = "Place"
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label5.Location = New System.Drawing.Point(142, 223)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(43, 20)
+        Me.label5.TabIndex = 38
+        Me.label5.Text = "Time"
+        '
+        'lbl
+        '
+        Me.lbl.AutoSize = True
+        Me.lbl.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl.Location = New System.Drawing.Point(142, 180)
+        Me.lbl.Name = "lbl"
+        Me.lbl.Size = New System.Drawing.Size(52, 20)
+        Me.lbl.TabIndex = 37
+        Me.lbl.Text = "Stage"
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label4.Location = New System.Drawing.Point(142, 146)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(63, 20)
+        Me.label4.TabIndex = 36
+        Me.label4.Text = "Gender"
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label3.Location = New System.Drawing.Point(142, 112)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(56, 20)
+        Me.label3.TabIndex = 35
+        Me.label3.Text = "Stroke"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(142, 78)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(72, 20)
+        Me.label2.TabIndex = 34
+        Me.label2.Text = "Distance"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(140, 44)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(74, 20)
+        Me.label1.TabIndex = 33
+        Me.label1.Text = "Swimmer"
+        '
+        'cmbStage
+        '
+        Me.cmbStage.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbStage.FormattingEnabled = True
+        Me.cmbStage.Location = New System.Drawing.Point(242, 180)
+        Me.cmbStage.Name = "cmbStage"
+        Me.cmbStage.Size = New System.Drawing.Size(204, 28)
+        Me.cmbStage.TabIndex = 32
+        '
+        'cmbGender
+        '
+        Me.cmbGender.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbGender.FormattingEnabled = True
+        Me.cmbGender.Location = New System.Drawing.Point(242, 146)
+        Me.cmbGender.Name = "cmbGender"
+        Me.cmbGender.Size = New System.Drawing.Size(204, 28)
+        Me.cmbGender.TabIndex = 31
+        '
+        'cmbStroke
+        '
+        Me.cmbStroke.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbStroke.FormattingEnabled = True
+        Me.cmbStroke.Location = New System.Drawing.Point(242, 112)
+        Me.cmbStroke.Name = "cmbStroke"
+        Me.cmbStroke.Size = New System.Drawing.Size(204, 28)
+        Me.cmbStroke.TabIndex = 30
+        '
+        'cmbDistance
+        '
+        Me.cmbDistance.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbDistance.FormattingEnabled = True
+        Me.cmbDistance.Location = New System.Drawing.Point(242, 78)
+        Me.cmbDistance.Name = "cmbDistance"
+        Me.cmbDistance.Size = New System.Drawing.Size(204, 28)
+        Me.cmbDistance.TabIndex = 29
+        '
+        'cmbSwimmer
+        '
+        Me.cmbSwimmer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbSwimmer.FormattingEnabled = True
+        Me.cmbSwimmer.Location = New System.Drawing.Point(242, 44)
+        Me.cmbSwimmer.Name = "cmbSwimmer"
+        Me.cmbSwimmer.Size = New System.Drawing.Size(204, 28)
+        Me.cmbSwimmer.TabIndex = 28
+        '
+        'button5
+        '
+        Me.button5.Location = New System.Drawing.Point(452, 298)
+        Me.button5.Name = "button5"
+        Me.button5.Size = New System.Drawing.Size(63, 39)
+        Me.button5.TabIndex = 27
+        Me.button5.Text = "Cancel"
+        Me.button5.UseVisualStyleBackColor = True
+        '
+        'button4
+        '
+        Me.button4.Location = New System.Drawing.Point(383, 298)
+        Me.button4.Name = "button4"
+        Me.button4.Size = New System.Drawing.Size(63, 39)
+        Me.button4.TabIndex = 26
+        Me.button4.Text = "Save"
+        Me.button4.UseVisualStyleBackColor = True
+        '
+        'button3
+        '
+        Me.button3.Location = New System.Drawing.Point(314, 298)
+        Me.button3.Name = "button3"
+        Me.button3.Size = New System.Drawing.Size(63, 39)
+        Me.button3.TabIndex = 25
+        Me.button3.Text = "Delete"
+        Me.button3.UseVisualStyleBackColor = True
+        '
+        'button2
+        '
+        Me.button2.Location = New System.Drawing.Point(245, 298)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(63, 39)
+        Me.button2.TabIndex = 24
+        Me.button2.Text = "Edit"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(176, 298)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(63, 39)
+        Me.button1.TabIndex = 23
+        Me.button1.Text = "Add"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'txtPlace
+        '
+        Me.txtPlace.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPlace.Location = New System.Drawing.Point(242, 249)
+        Me.txtPlace.Name = "txtPlace"
+        Me.txtPlace.Size = New System.Drawing.Size(204, 29)
+        Me.txtPlace.TabIndex = 22
+        '
+        'txtTime
+        '
+        Me.txtTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTime.Location = New System.Drawing.Point(242, 214)
+        Me.txtTime.Name = "txtTime"
+        Me.txtTime.Size = New System.Drawing.Size(204, 29)
+        Me.txtTime.TabIndex = 21
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(710, 613)
+        Me.Controls.Add(Me.dataGridView1)
+        Me.Controls.Add(Me.label6)
+        Me.Controls.Add(Me.label5)
+        Me.Controls.Add(Me.lbl)
+        Me.Controls.Add(Me.label4)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.cmbStage)
+        Me.Controls.Add(Me.cmbGender)
+        Me.Controls.Add(Me.cmbStroke)
+        Me.Controls.Add(Me.cmbDistance)
+        Me.Controls.Add(Me.cmbSwimmer)
+        Me.Controls.Add(Me.button5)
+        Me.Controls.Add(Me.button4)
+        Me.Controls.Add(Me.button3)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.txtPlace)
+        Me.Controls.Add(Me.txtTime)
+        Me.Name = "Form1"
+        Me.Text = "Result"
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents dataGridView1 As System.Windows.Forms.DataGridView
+    Private WithEvents label6 As System.Windows.Forms.Label
+    Private WithEvents label5 As System.Windows.Forms.Label
+    Private WithEvents lbl As System.Windows.Forms.Label
+    Private WithEvents label4 As System.Windows.Forms.Label
+    Private WithEvents label3 As System.Windows.Forms.Label
+    Private WithEvents label2 As System.Windows.Forms.Label
+    Private WithEvents label1 As System.Windows.Forms.Label
+    Private WithEvents cmbStage As System.Windows.Forms.ComboBox
+    Private WithEvents cmbGender As System.Windows.Forms.ComboBox
+    Private WithEvents cmbStroke As System.Windows.Forms.ComboBox
+    Private WithEvents cmbDistance As System.Windows.Forms.ComboBox
+    Private WithEvents cmbSwimmer As System.Windows.Forms.ComboBox
+    Private WithEvents button5 As System.Windows.Forms.Button
+    Private WithEvents button4 As System.Windows.Forms.Button
+    Private WithEvents button3 As System.Windows.Forms.Button
+    Private WithEvents button2 As System.Windows.Forms.Button
+    Private WithEvents button1 As System.Windows.Forms.Button
+    Private WithEvents txtPlace As System.Windows.Forms.TextBox
+    Private WithEvents txtTime As System.Windows.Forms.TextBox
+
+End Class
---
+++ VB_SQLITE/Form1.vb
@@ -0,0 +1,236 @@
+﻿Imports System.Data.SQLite
+Public Class Form1
+
+    Dim conn As SQLiteConnection = New SQLiteConnection("Data Source=swimmer.db;Version=3;FailIfMissing=True;")
+    Dim save_sta As String = ""
+    Dim a, b, c, d, f As String
+
+    Sub showdata()
+        conn.Open()
+        Dim sql As String
+        sql = "select (Swimer.Fname ||' '|| Swimer.Lname) as 'Swimmer',cast(Distance.Length as text) as 'Length',Stroke.Stroke_ as 'Stroke',Gender.Gender_ as 'Gender',Stage.Stage_ as 'Stage',Result.Time,Result.Place "
+        sql += "FROM Swimer,Gender,Result,Stage,Stroke,Distance "
+        sql += "where Result.Swimer = Swimer.Swimer_ID and Result.Gender = Gender.Gender_ID and Result.Distance = Distance.Distance_ID "
+        sql += "and Result.Stage = Stage.Stage_ID and Result.Stroke = Stroke.Stroke_ID order by Swimer.Swimer_ID"
+        Dim da As SQLiteDataAdapter = New SQLiteDataAdapter(sql, conn)
+        Dim ds As DataSet = New DataSet()
+        da.Fill(ds, "result")
+        dataGridView1.DataSource = ds
+        dataGridView1.DataMember = "result"
+        dataGridView1.ReadOnly = True
+        conn.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        showdata()
+        cmb_exe()
+        button2.Enabled = False
+        button3.Enabled = False
+        button4.Enabled = False
+    End Sub
+
+    Sub sql_exe(ByVal sql As String)
+        conn.Open()
+        Dim sqlcdm As SQLiteCommand = New SQLiteCommand(sql, conn)
+        sqlcdm.ExecuteNonQuery()
+        conn.Close()
+    End Sub
+
+    Sub cmb_exe()
+        conn.Open()
+        Dim da1 As SQLiteDataAdapter = New SQLiteDataAdapter("select Swimer_ID,(Fname ||' '|| Lname) as 'name' from Swimer", conn)
+        Dim da2 As SQLiteDataAdapter = New SQLiteDataAdapter("select Distance_ID,cast(Length as text) as Length from Distance", conn)
+        Dim da3 As SQLiteDataAdapter = New SQLiteDataAdapter("select * from Gender", conn)
+        Dim da4 As SQLiteDataAdapter = New SQLiteDataAdapter("select * from Stage", conn)
+        Dim da5 As SQLiteDataAdapter = New SQLiteDataAdapter("select * from Stroke", conn)
+        Dim ds1 As DataSet = New DataSet()
+        da1.Fill(ds1, "d1")
+        da2.Fill(ds1, "d2")
+        da3.Fill(ds1, "d3")
+        da4.Fill(ds1, "d4")
+        da5.Fill(ds1, "d5")
+        conn.Close()
+        cmbDistance.DataSource = ds1.Tables("d2")
+        cmbSwimmer.DataSource = ds1.Tables("d1")
+        cmbGender.DataSource = ds1.Tables("d3")
+        cmbStage.DataSource = ds1.Tables("d4")
+        cmbStroke.DataSource = ds1.Tables("d5")
+
+        cmbDistance.ValueMember = "Distance_ID"
+        cmbSwimmer.ValueMember = "Swimer_ID"
+        cmbGender.ValueMember = "Gender_ID"
+        cmbStage.ValueMember = "Stage_ID"
+        cmbStroke.ValueMember = "Stroke_ID"
+
+        cmbDistance.DisplayMember = "Length"
+        cmbSwimmer.DisplayMember = "name"
+        cmbGender.DisplayMember = "Gender_"
+        cmbStage.DisplayMember = "Stage_"
+        cmbStroke.DisplayMember = "Stroke_"
+
+        cmbDistance.SelectedIndex = -1
+        cmbGender.SelectedIndex = -1
+        cmbStage.SelectedIndex = -1
+        cmbStroke.SelectedIndex = -1
+        cmbSwimmer.SelectedIndex = -1
+
+        cmbDistance.Enabled = False
+        cmbGender.Enabled = False
+        cmbStage.Enabled = False
+        cmbStroke.Enabled = False
+        cmbSwimmer.Enabled = False
+
+        txtPlace.Text = ""
+        txtTime.Text = ""
+        txtPlace.Enabled = False
+        txtTime.Enabled = False
+    End Sub
+
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        button1.Enabled = False
+        button4.Enabled = True
+
+        cmbDistance.SelectedIndex = -1
+        cmbGender.SelectedIndex = -1
+        cmbStage.SelectedIndex = -1
+        cmbStroke.SelectedIndex = -1
+        cmbSwimmer.SelectedIndex = -1
+
+        txtPlace.Text = ""
+        txtTime.Text = ""
+
+        cmbDistance.Enabled = True
+        cmbGender.Enabled = True
+        cmbStage.Enabled = True
+        cmbStroke.Enabled = True
+        cmbSwimmer.Enabled = True
+        txtPlace.Enabled = True
+        txtTime.Enabled = True
+
+        save_sta = "add"
+    End Sub
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles button2.Click
+        save_sta = "edit"
+        button4.Enabled = True
+        button2.Enabled = False
+        button3.Enabled = False
+        cmbDistance.Enabled = True
+        cmbGender.Enabled = True
+        cmbStage.Enabled = True
+        cmbStroke.Enabled = True
+        cmbSwimmer.Enabled = True
+        txtPlace.Enabled = True
+        txtTime.Enabled = True
+    End Sub
+
+    Private Sub button4_Click(sender As Object, e As EventArgs) Handles button4.Click
+        If (cmbDistance.SelectedIndex = -1 Or cmbGender.SelectedIndex = -1 Or cmbStage.SelectedIndex = -1 Or cmbStroke.SelectedIndex = -1 Or cmbSwimmer.SelectedIndex = -1 Or txtPlace.Text = Nothing Or txtTime.Text = Nothing) Then
+            Exit Sub
+        End If
+        If (save_sta = "add") Then
+            sql_exe("insert into Result values ('" + CStr(cmbSwimmer.SelectedValue) + "','" + CStr(cmbDistance.SelectedValue) + "','" + CStr(cmbStroke.SelectedValue) + "','" + CStr(cmbGender.SelectedValue) + "','" + CStr(cmbStage.SelectedValue) + "','" + CStr(txtTime.Text) + "','" + CStr(txtPlace.Text) + "')")
+        End If
+
+
+        If (save_sta = "edit") Then
+            Dim sql As String
+            sql = "update Result set Swimer = '" + cmbSwimmer.SelectedValue + "',Distance = '" + cmbDistance.SelectedValue + "',Stroke = '" + cmbStroke.SelectedValue + "',Gender = '" + cmbGender.SelectedValue + "',Stage = '" + cmbStage.SelectedValue + "',"
+            sql += "Time = '" + txtTime.Text + "',Place = '" + txtPlace.Text + "' where "
+            sql += "Swimer = '" + a + "' and Distance = '" + b + "' and Stroke = '" + c + "' and Gender = '" + d + "' and Stage = '" + f + "'"
+            sql_exe(sql)
+        End If
+        showdata()
+        button1.Enabled = True
+        button2.Enabled = False
+        button3.Enabled = False
+        button4.Enabled = False
+
+        cmbDistance.SelectedIndex = -1
+        cmbGender.SelectedIndex = -1
+        cmbStage.SelectedIndex = -1
+        cmbStroke.SelectedIndex = -1
+        cmbSwimmer.SelectedIndex = -1
+
+        cmbDistance.Enabled = False
+        cmbGender.Enabled = False
+        cmbStage.Enabled = False
+        cmbStroke.Enabled = False
+        cmbSwimmer.Enabled = False
+
+        txtPlace.Text = ""
+        txtTime.Text = ""
+        txtPlace.Enabled = False
+        txtTime.Enabled = False
+    End Sub
+
+    Private Sub button3_Click(sender As Object, e As EventArgs) Handles button3.Click
+        sql_exe("delete from Result where Swimer = '" + a + "' and Distance = '" + b + "' and Stroke = '" + c + "' and Gender = '" + d + "' and Stage = '" + f + "'")
+        showdata()
+        button1.Enabled = True
+        button2.Enabled = False
+        button3.Enabled = False
+        button4.Enabled = False
+
+        cmbDistance.SelectedIndex = -1
+        cmbGender.SelectedIndex = -1
+        cmbStage.SelectedIndex = -1
+        cmbStroke.SelectedIndex = -1
+        cmbSwimmer.SelectedIndex = -1
+
+        cmbDistance.Enabled = False
+        cmbGender.Enabled = False
+        cmbStage.Enabled = False
+        cmbStroke.Enabled = False
+        cmbSwimmer.Enabled = False
+
+        txtPlace.Text = ""
+        txtTime.Text = ""
+        txtPlace.Enabled = False
+        txtTime.Enabled = False
+    End Sub
+
+    Private Sub dataGridView1_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dataGridView1.CellContentDoubleClick
+        cmbSwimmer.Text = dataGridView1.Rows(e.RowIndex).Cells(0).Value.ToString()
+        cmbDistance.Text = dataGridView1.Rows(e.RowIndex).Cells(1).Value.ToString()
+        cmbStroke.Text = dataGridView1.Rows(e.RowIndex).Cells(2).Value.ToString()
+        cmbGender.Text = dataGridView1.Rows(e.RowIndex).Cells(3).Value.ToString()
+        cmbStage.Text = dataGridView1.Rows(e.RowIndex).Cells(4).Value.ToString()
+        txtTime.Text = dataGridView1.Rows(e.RowIndex).Cells(5).Value.ToString()
+        txtPlace.Text = dataGridView1.Rows(e.RowIndex).Cells(6).Value.ToString()
+        a = cmbSwimmer.SelectedValue.ToString()
+        b = cmbDistance.SelectedValue.ToString()
+        c = cmbStroke.SelectedValue.ToString()
+        d = cmbGender.SelectedValue.ToString()
+        f = cmbStage.SelectedValue.ToString()
+
+        button1.Enabled = False
+        button2.Enabled = True
+        button3.Enabled = True
+        button4.Enabled = False
+    End Sub
+
+    Private Sub button5_Click(sender As Object, e As EventArgs) Handles button5.Click
+        button1.Enabled = True
+        button2.Enabled = False
+        button3.Enabled = False
+        button4.Enabled = False
+
+        cmbDistance.SelectedIndex = -1
+        cmbGender.SelectedIndex = -1
+        cmbStage.SelectedIndex = -1
+        cmbStroke.SelectedIndex = -1
+        cmbSwimmer.SelectedIndex = -1
+
+        cmbDistance.Enabled = False
+        cmbGender.Enabled = False
+        cmbStage.Enabled = False
+        cmbStroke.Enabled = False
+        cmbSwimmer.Enabled = False
+
+        txtPlace.Text = ""
+        txtTime.Text = ""
+        txtPlace.Enabled = False
+        txtTime.Enabled = False
+    End Sub
+End Class
